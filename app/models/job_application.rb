794283b2e1576c502ee949e80d90c23d7bc217f6
apple <apple@MacBook-Pro.local>
2025-10-30 18:36:25 +0530

    add all files

--- app/models/job_application.rb
+++ app/models/job_application.rb
@@ -5,10 +5,4 @@ class JobApplication < ApplicationRecord
   validates :about_yourself, presence: true
   validates :resume, presence: true
   validates :job_id, uniqueness: { scope: :jobseeker_id, message: "already applied" }
-  # enum status: { 
-  #   review: "review", 
-  #   reject: "reject",
-  #   selected_for_interview: "selected_for_interview", 
-  #   selected_for_job: "selected_for_job" 
-  # }
 end
